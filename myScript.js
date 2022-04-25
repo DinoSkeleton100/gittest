cbeb6b076f6b06f9a2d316650ec5b47f7814099f
Alex de Roos <100049383+DinoSkeleton100@users.noreply.github.com>
2022-04-25 15:44:22 +0200

    per card animation

--- myScript.js
+++ myScript.js
@@ -3,10 +3,13 @@ $(document).ready(function() {
     $('.cardContent').slideToggle();
 
     $('.cardTitle').on('click', function() {
-        $('.cardContent').slideToggle(500);
-        $('.card').slideToggle(500);
-        $('.card').toggleClass('cardBig');
-        $('.card').slideToggle();
+
+        var cardId = $(this).attr('data-cardId');
+
+        $('#content'+cardId).slideToggle(500);
+        $('#'+cardId).slideToggle(500);
+        $('#'+cardId).toggleClass('cardBig');
+        $('#'+cardId).slideToggle();
     });
 
 });
