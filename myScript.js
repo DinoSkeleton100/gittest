98a089844ec75de6c66d6883f0fe6459e0835f0e
Alex de Roos <100049383+DinoSkeleton100@users.noreply.github.com>
2022-04-25 22:16:43 +0200

    new animation with content background

--- myScript.js
+++ myScript.js
@@ -7,9 +7,6 @@ $(document).ready(function() {
         var cardId = $(this).attr('data-cardId');
 
         $('#content'+cardId).slideToggle(500);
-        $('#'+cardId).slideToggle(500);
-        $('#'+cardId).toggleClass('cardBig');
-        $('#'+cardId).slideToggle();
     });
 
 });
